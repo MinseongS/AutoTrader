200d9fd830f284fd330620e41b2f8bacfad90aa7
MinseongS <subaksms@smartradarsystem.com>
2022-08-19 16:02:41 +0900

    chage false commit

--- my_auto_trader.py
+++ my_auto_trader.py
@@ -2,6 +2,7 @@ import telegram
 import pyupbit
 import time
 import schedule
+import datetime
 
 
 # ## 텔레그램 토큰키와 챗아이디를 가지고 있으면 아래의 각각 항목에 대입해주고
@@ -12,26 +13,39 @@ import schedule
 # def post_message(text):
 #     bot.sendMessage(chat_id=chat_id, text=text)  # 텔레그램 알림으로 변경함.
 #     print(text)
-#
-#
+
+
 access = "Dddyixi3RpVjVD4XfIyP7xmIebpfKU1Xt4FKumpE"  # upbit 에서 받은 본인의 엑세스키  넣으세요.
 secret = "aGItZYSG3xZJu1MCAbjpSBwfejMMTqFHQsDRBf4a"  # upbit 에서 받은 본인의 시크릿키  넣으세요.
-#
+
+
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-#
-# # 임의로 5개 코인을 골랐으니 각자 맘에 드는 코인으로 변경해서 사용하세요.
-# # 갯수는 상관 없습니다만 현금 시드가 확보되어 있어야 매수가 되겠지요...^^;
-# # 아래는 샘플로 도지코인, 리플코인, 이더리움 코인, 이더리움 클래식 코인, 에이다 코인임
-coins = ['BTC', 'ETH']
-ratio = [0.6, 0.2]
+
+coins = {'BTC': 0.6, 'ETH': 0.2}
 #
 #
 balances = upbit.get_balances()
 print(balances)
-cur_price = pyupbit.get_current_price('KRW-BTC')
-print(cur_price)
-def get_asset():
+
+def trade():
+    cur_val = {coin: 0 for coin in coins.keys()}
+    asset = get_asset(cur_val)
+    now = time.localtime()
+    print("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
+    print(f"Total value : {asset:,.0f}")
+    for coin in coins.keys():
+        if cur_val[coin] < coins[coin] * asset:
+            # res = upbit.buy_market_order(coin, coins[coin] * asset - cur_val[coin])
+            temp = 'KRW-' + coin
+            print(f"buy \t{coin} \t{coins[coin] * asset - cur_val[coin]:,.0f} \t{pyupbit.get_current_price(temp):,.0f}")
+        else:
+            # res = upbit.sell_market_order(coin, cur_val[coin] - coins[coin] * asset)
+            temp = 'KRW-' + coin
+            print(f"sell \t{coin} \t{cur_val[coin]- coins[coin] * asset:,.0f} \t{pyupbit.get_current_price(temp):,.0f}")
+    return
+
+def get_asset(cur_val):
     balances = upbit.get_balances()
     total = 0
     for b in balances:
@@ -41,10 +55,12 @@ def get_asset():
             coin = b['currency']
             ticker = 'KRW-' + coin
             cur_price = pyupbit.get_current_price(ticker)
-            total += cur_price * float(b['balance'])
+            temp = cur_price * float(b['balance'])
+            total += temp
+            cur_val[coin] = temp
     return total
-print(get_asset())
 
+trade()
 # # 해당 코인 보유수량 반환
 # def get_balance(coin):
 #     """잔고 조회"""
